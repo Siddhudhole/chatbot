9e626894deb43493883307f91d293313ebef0d8d
Siddharth dhole <113662425+Siddhudhole@users.noreply.github.com>
2024-09-07 13:31:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,20 @@
 import streamlit as st
+from transformers import pipeline,AutoModelForCausalLM,AutoTokenizer  
 
-st.title('🎈 App Name')
 
-st.write('Hello world!')
+model_check_point = 'gpt2'
+
+llm = TFAutoModelForCausalLM.from_pretrained(model_check_point)
+tokenizer = AutoTokenizer.from_pretrained(model_check_point) 
+pipe = pipeline(tokenizer=tokenizer,model=llm) 
+
+
+
+
+st.title('Chat Bot Service') 
+input_text = st.text_input('Search the topic u want')    
+
+if input_text:
+    input_prompt = f'Question: {input_text}'
+    response = pipe(input_prompt, max_length=512, num_return_sequences=1)
+    st.write(response[0].strip()) 
