c678adffecf8014c641b957791c19558ac1b89b9
Siddharth dhole <113662425+Siddhudhole@users.noreply.github.com>
2024-09-07 13:35:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,12 +9,16 @@ tokenizer = AutoTokenizer.from_pretrained(model_check_point)
 pipe = pipeline(tokenizer=tokenizer,model=llm) 
 
 
-
+def output(input_text):
+    input_prompt = f'Question: {input_text}'
+    response = pipe(input_prompt, max_length=512, num_return_sequences=1)
+    return response 
+    
+    
 
 st.title('Chat Bot Service') 
 input_text = st.text_input('Search the topic u want')    
 
 if input_text:
-    input_prompt = f'Question: {input_text}'
-    response = pipe(input_prompt, max_length=512, num_return_sequences=1)
+    response=output(input_text)
     st.write(response[0].strip()) 
